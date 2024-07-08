4ab8007b53afe4cf581b49044cab26f61678adbf
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-07-09 00:47:34 +1000

    Can change editor context. no longer using TipTap doc (cloud)

--- src/components/BlockEditor/BlockEditor.tsx
+++ src/components/BlockEditor/BlockEditor.tsx
@@ -1,44 +1,61 @@
-'use client'
+"use client";
 
-import { EditorContent, PureEditorContent } from '@tiptap/react'
-import React, { useMemo, useRef } from 'react'
+import { EditorContent, PureEditorContent, useEditor } from "@tiptap/react";
+import React, { useMemo, useRef } from "react";
 
-import { LinkMenu } from '@/components/menus'
+import { LinkMenu } from "@/components/menus";
 
-import { useBlockEditor } from '@/hooks/useBlockEditor'
+import { useBlockEditor } from "@/hooks/useBlockEditor";
 
-import '@/styles/index.css'
+import "@/styles/index.css";
 
-import { Sidebar } from '@/components/Sidebar'
-import { EditorContext } from '@/context/EditorContext'
-import ImageBlockMenu from '@/extensions/ImageBlock/components/ImageBlockMenu'
-import { ColumnsMenu } from '@/extensions/MultiColumn/menus'
-import { TableColumnMenu, TableRowMenu } from '@/extensions/Table/menus'
-import { TiptapProps } from './types'
-import { EditorHeader } from './components/EditorHeader'
-import { TextMenu } from '../menus/TextMenu'
-import { ContentItemMenu } from '../menus/ContentItemMenu'
+import { Sidebar } from "@/components/Sidebar";
+import { EditorContext } from "@/context/EditorContext";
+import ImageBlockMenu from "@/extensions/ImageBlock/components/ImageBlockMenu";
+import { ColumnsMenu } from "@/extensions/MultiColumn/menus";
+import { TableColumnMenu, TableRowMenu } from "@/extensions/Table/menus";
+import { TiptapProps } from "./types";
+import { EditorHeader } from "./components/EditorHeader";
+import { TextMenu } from "../menus/TextMenu";
+import { ContentItemMenu } from "../menus/ContentItemMenu";
+import { initialContent } from "@/lib/data/initialContent";
+import { blankContent } from "@/lib/data/blankContent";
+import ExtensionKit from "@/extensions/extension-kit";
 
 export const BlockEditor = ({ ydoc, provider }: TiptapProps) => {
-  const menuContainerRef = useRef(null)
-  const editorRef = useRef<PureEditorContent | null>(null)
+  const menuContainerRef = useRef(null);
+  const editorRef = useRef<PureEditorContent | null>(null);
 
-  const { editor, users, characterCount, collabState, leftSidebar } = useBlockEditor({ ydoc, provider })
+  const { editor, users, characterCount, collabState, leftSidebar } =
+    useBlockEditor({ ydoc, provider });
 
-  const displayedUsers = users.slice(0, 3)
+  const customEditor! = useEditor({
+    extensions: [
+      ...ExtensionKit({
+        provider,
+      }),
+    ],
+    content: blankContent,
+  });
+
+  const displayedUsers = users.slice(0, 3);
 
   const providerValue = useMemo(() => {
-    return {}
-  }, [])
+    return {};
+  }, []);
 
   if (!editor) {
-    return null
+    return null;
   }
 
   return (
     <EditorContext.Provider value={providerValue}>
       <div className="flex h-full" ref={menuContainerRef}>
-        <Sidebar isOpen={leftSidebar.isOpen} onClose={leftSidebar.close} editor={editor} />
+        <Sidebar
+          isOpen={leftSidebar.isOpen}
+          onClose={leftSidebar.close}
+          editor={editor}
+        />
         <div className="relative flex flex-col flex-1 h-full overflow-hidden">
           <EditorHeader
             characters={characterCount.characters()}
@@ -48,18 +65,22 @@ export const BlockEditor = ({ ydoc, provider }: TiptapProps) => {
             isSidebarOpen={leftSidebar.isOpen}
             toggleSidebar={leftSidebar.toggle}
           />
-          <EditorContent editor={editor} ref={editorRef} className="flex-1 overflow-y-auto" />
-          <ContentItemMenu editor={editor} />
-          <LinkMenu editor={editor} appendTo={menuContainerRef} />
-          <TextMenu editor={editor} />
-          <ColumnsMenu editor={editor} appendTo={menuContainerRef} />
-          <TableRowMenu editor={editor} appendTo={menuContainerRef} />
-          <TableColumnMenu editor={editor} appendTo={menuContainerRef} />
-          <ImageBlockMenu editor={editor} appendTo={menuContainerRef} />
+          <EditorContent
+            editor={customEditor}
+            ref={editorRef}
+            className="flex-1 overflow-y-auto"
+          />
+          <ContentItemMenu editor={customEditor!} />
+          <LinkMenu editor={customEditor!} appendTo={menuContainerRef} />
+          <TextMenu editor={customEditor!} />
+          <ColumnsMenu editor={customEditor!} appendTo={menuContainerRef} />
+          <TableRowMenu editor={customEditor!} appendTo={menuContainerRef} />
+          <TableColumnMenu editor={customEditor!} appendTo={menuContainerRef} />
+          <ImageBlockMenu editor={customEditor!} appendTo={menuContainerRef} />
         </div>
       </div>
     </EditorContext.Provider>
-  )
-}
+  );
+};
 
-export default BlockEditor
+export default BlockEditor;
---
+++ src/lib/data/blankContent.tsx
@@ -0,0 +1,18 @@
+export const blankContent = {
+  type: "doc",
+  content: [
+    {
+      type: "paragraph",
+      attrs: {
+        class: null,
+        textAlign: "left",
+      },
+      content: [
+        {
+          type: "text",
+          text: "Welcome to our React Block Editor Template built on top of ",
+        },
+      ],
+    },
+  ],
+};
