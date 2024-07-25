70139437e390f00fe5ab9263ceb001479fa595f0
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-07-25 18:26:37 +1000

    Mnemonic can now be copied by users.

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -28,117 +28,67 @@ import { Surface } from "@/components/ui/Surface";
 import { Toolbar } from "@/components/ui/Toolbar";
 import { Icon } from "@/components/ui/Icon";
 import { useRouter } from "next/navigation";
-
-const useDarkmode = () => {
-  const [isDarkMode, setIsDarkMode] = React.useState<boolean>(
-    typeof window !== "undefined"
-      ? window.matchMedia("(prefers-color-scheme: dark)").matches
-      : false,
-  );
-
-  React.useEffect(() => {
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    const handleChange = () => setIsDarkMode(mediaQuery.matches);
-    mediaQuery.addEventListener("change", handleChange);
-    return () => mediaQuery.removeEventListener("change", handleChange);
-  }, []);
-
-  React.useEffect(() => {
-    document.documentElement.classList.toggle("dark", isDarkMode);
-  }, [isDarkMode]);
-
-  const toggleDarkMode = React.useCallback(
-    () => setIsDarkMode((isDark) => !isDark),
-    [],
-  );
-  const lightMode = React.useCallback(() => setIsDarkMode(false), []);
-  const darkMode = React.useCallback(() => setIsDarkMode(true), []);
-
-  return {
-    isDarkMode,
-    toggleDarkMode,
-    lightMode,
-    darkMode,
-  };
-};
+import { Label } from "@/components/ui/label";
+import Copy from "@/components/copy/CopyLink";
+import CopySecret from "@/components/copy/CopySecret";
+import { EvoluProvider } from "@evolu/react";
+import { evolu } from "@/db/db";
 
 export default function Settings() {
-  const { isDarkMode, darkMode, lightMode } = useDarkmode();
-
-  // darkMode();
-  //
-  const DarkModeSwitcher = createPortal(
-    <Surface className="flex items-center gap-1 fixed bottom-6 right-6 z-[99999] p-1">
-      <Toolbar.Button onClick={lightMode} active={!isDarkMode}>
-        <Icon name="Sun" />
-      </Toolbar.Button>
-      <Toolbar.Button onClick={darkMode} active={isDarkMode}>
-        <Icon name="Moon" />
-      </Toolbar.Button>
-    </Surface>,
-    document.body,
-  );
-
   const router = useRouter();
 
   return (
-    <div className="flex min-h-screen w-full flex-col">
-      {DarkModeSwitcher}
-      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
-        <div
-          className="mx-auto flex flex-row items-center w-full max-w-6xl gap-4"
-          onClick={() => router.back()}
-        >
-          <ArrowLeft />
-          <h1 className="text-3xl font-semibold">Settings</h1>
-        </div>
-        <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-          <nav
-            className="grid gap-4 text-sm text-muted-foreground"
-            x-chunk="dashboard-04-chunk-0"
+    <EvoluProvider value={evolu}>
+      <div className="flex min-h-screen w-full flex-col">
+        <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+          <div
+            className="mx-auto flex flex-row items-center w-full max-w-6xl gap-4"
+            onClick={() => router.back()}
           >
-            <Link href="#" className="font-semibold text-primary">
-              General
-            </Link>
-            {/* <Link href="#">Security</Link>
+            <ArrowLeft />
+            <h1 className="text-3xl font-semibold">Settings</h1>
+          </div>
+          <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+            <nav
+              className="grid gap-4 text-sm text-muted-foreground"
+              x-chunk="dashboard-04-chunk-0"
+            >
+              <Link href="#" className="font-semibold text-primary">
+                General
+              </Link>
+              {/* <Link href="#">Security</Link>
             <Link href="#">Integrations</Link>
             <Link href="#">Support</Link>
             <Link href="#">Organizations</Link> */}
-            <Link href="/settings/advanced">Advanced</Link>
-          </nav>
-          <div className="grid gap-6">
-            <Card x-chunk="dashboard-04-chunk-1">
-              <CardHeader>
-                <CardTitle>Plugins Directory</CardTitle>
-                <CardDescription>
-                  The directory within your project, in which your plugins are
-                  located.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <form className="flex flex-col gap-4">
-                  <Input
-                    placeholder="Project Name"
-                    defaultValue="/content/plugins"
-                  />
-                  <div className="flex items-center space-x-2">
-                    {/* <Checkbox id="include" defaultChecked /> */}
-                    <label
-                      htmlFor="include"
-                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                    >
-                      Allow administrators to change the directory.
-                    </label>
+              <Link href="/settings/advanced">Advanced</Link>
+            </nav>
+            <div className="grid gap-6">
+              <Card x-chunk="dashboard-04-chunk-1">
+                <CardHeader>
+                  <CardTitle>Account</CardTitle>
+                  <CardDescription>Account related settings.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <div className="flex flex-col gap-4">
+                    <Label htmlFor="mnemonic">Mnemonic:</Label>
+                    <CopySecret />
+                    <div className="flex items-center space-x-2">
+                      {/* <Checkbox id="include" defaultChecked /> */}
+                      <label
+                        htmlFor="include"
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      >
+                        This is what allows you to sync your encrypted notes, DO
+                        NOT SHARE.
+                      </label>
+                    </div>
                   </div>
-                </form>
-              </CardContent>
-              <CardFooter className="border-t px-6 py-4">
-                <Button>Save</Button>
-              </CardFooter>
-            </Card>
+                </CardContent>
+              </Card>
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </EvoluProvider>
   );
 }
