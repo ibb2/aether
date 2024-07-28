2889f95243228142bf5f56baf15e8955706cd17d
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-07-28 18:22:14 +1000

    Deleted items no longer show up in Arborist view.

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,15 +1,23 @@
+import * as S from "@effect/schema/Schema";
 import { evolu } from "./db";
+import { cast } from "@evolu/react";
 
 export const notebooksQuery = evolu.createQuery((db) =>
-  db.selectFrom("notebooks").selectAll(),
+  db
+    .selectFrom("notebooks")
+    .where("isDeleted", "is not", cast(true))
+    .selectAll(),
 );
 
 export const sectionsQuery = evolu.createQuery((db) =>
-  db.selectFrom("sections").selectAll(),
+  db
+    .selectFrom("sections")
+    .where("isDeleted", "is not", cast(true))
+    .selectAll(),
 );
 
 export const notesQuery = evolu.createQuery((db) =>
-  db.selectFrom("notes").selectAll(),
+  db.selectFrom("notes").where("isDeleted", "is not", cast(true)).selectAll(),
 );
 
 export const initialExportedData = evolu.createQuery((db) =>
