3a497231b433afae395b59820375e7e21e721435
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-08-26 21:17:06 +1000

    Updated database schema to include note fragments.

--- src/db/db.ts
+++ src/db/db.ts
@@ -14,6 +14,7 @@ import {
 } from "@evolu/react";
 import {
   ExportedDatasTable,
+  FragmentsTable,
   NonEmptyString50,
   NotebooksTable,
   NoteSettingId,
@@ -31,6 +32,7 @@ const Database = database({
   users: UsersTable,
   notebooks: NotebooksTable,
   sections: SectionsTable,
+  fragments: FragmentsTable,
   notes: NotesTable,
   noteSettings: NotesSettingsTable,
   exportedData: ExportedDatasTable,
