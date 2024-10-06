290c8fae284503f6f0d2ee8c5dc990d75281c11d
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-10-06 14:45:25 +1100

    Analytics toggle is functional.

--- src/app/settings/privacy/page.tsx
+++ src/app/settings/privacy/page.tsx
@@ -10,37 +10,41 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
 import { evolu } from "@/db/db";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Privacy() {
-  const router = useRouter();
+  const [render, onRender] = React.useState<boolean>(true);
+  const [switchState, setSwitchState] = React.useState<boolean>(true);
+  const [analyticsEnabled, setAnalyticsEnabled] = React.useState(1);
 
-  const [saving, setSaving] = React.useState(false);
+  const handleAnalytics = () => {};
 
-  const uint8ArrayToBlob = (uint8Array: Uint8Array) => {
-    // SQLite files should use 'application/x-sqlite3' MIME type
-    return new Blob([uint8Array], { type: "application/x-sqlite3" });
-  };
+  React.useEffect(() => {
+    // Handles setting the user analytics
 
-  const download = async () => {
-    const data = await evolu.exportDatabase();
-    const blob = uint8ArrayToBlob(data);
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.style.display = "none";
-    a.href = url;
-    a.download = "aether.sqlite3";
-    document.body.appendChild(a);
-    a.click();
-    window.URL.revokeObjectURL(url);
-  };
+    // Get the analytics_allowed localstorage on page load to check it's value
+    // This is injected into the switch.
+    const analyticsChoice = window.localStorage.getItem("analytics_allowed");
 
-  const nuke = () => {
-    evolu.resetOwner({ reload: true });
-    // router.push("/app");
-  };
+    if (render) {
+      if (analyticsChoice !== null) {
+        setSwitchState(JSON.parse(analyticsChoice));
+        onRender(false);
+      }
+    }
+
+    // On switch change handle convert the bool val to int representation
+    // This is then stored into the analytics_allowed value for persistance across refreshes.
+    setAnalyticsEnabled(switchState ? 1 : 0);
+    window.localStorage.setItem(
+      "analytics_allowed",
+      JSON.stringify(analyticsEnabled ? 1 : 0),
+    );
+    console.log("Analytics ", switchState, analyticsChoice, render);
+  }, [switchState, analyticsEnabled, render]);
 
   return (
     <div className="flex min-h-screen w-full flex-col">
@@ -48,56 +52,25 @@ export default function Privacy() {
         <div className="grid gap-6">
           <Card x-chunk="dashboard-04-chunk-1" className="grid gap-3 ">
             <CardHeader>
-              <CardTitle>Account</CardTitle>
-              <CardDescription>Account related settings.</CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-x-20 gap-y-5">
-              <div className="flex flex-col justify-between w-full">
-                <div className="grid">
-                  <h1>Mnemonic</h1>
-                  <div className="flex items-center space-x-2">
-                    {/* <Checkbox id="include" defaultChecked /> */}
-                    <span className="text-sm font-light text-gray-400">
-                      Responsible for sync, DO NOT SHARE, DO NOT LOSE.
-                    </span>
-                  </div>
-                </div>
-                <CopySecret />
-              </div>
-            </CardContent>
-          </Card>
-          <Card x-chunk="dashboard-04-chunk-1" className="grid gap-3">
-            <CardHeader>
-              <CardTitle>Delete</CardTitle>
-              <CardDescription>Danger not reversible.</CardDescription>
+              <CardTitle>Privacy</CardTitle>
+              <CardDescription>Privacy related settings.</CardDescription>
             </CardHeader>
             <CardContent className="grid gap-x-20 gap-y-5">
               <div className="flex flex-col">
-                <div className="flex justify-between">
+                <div className="flex justify-between items-center">
                   <div className="grid">
                     <Label htmlFor="mnemonic" className="text-lg">
-                      Export
+                      Provide analytics
                     </Label>
                     <span className="text-sm font-light text-gray-400">
-                      Downloads the entire database.
+                      Basic analytics gathered via PostHog.
                     </span>
                   </div>
-                  <Button className="w-32" onClick={download}>
-                    Export Database
-                  </Button>
+                  <Switch
+                    checked={switchState}
+                    onCheckedChange={setSwitchState}
+                  />
                 </div>
-              </div>
-              <div className="flex flex-row justify-between">
-                <div className="grid">
-                  <h1>Delete Data</h1>
-                  <span className="text-sm font-light text-gray-400">
-                    Delete&apos;s all the data in the local database. A clean
-                    start.
-                  </span>
-                </div>
-                <Button variant="destructive" className="w-32" onClick={nuke}>
-                  Delete Data
-                </Button>
               </div>
             </CardContent>
           </Card>
