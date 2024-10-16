d8512547553ac6e010df1fbf6e7a8c8c062c08a8
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-10-16 21:25:25 +1100

    Posthog changed disabling, only works for client page for now.
    Same with the gathering of analytics. TODO is to add server components analytics.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,7 @@
 "use client";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
+import { useEffect, useState } from "react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
@@ -14,12 +15,25 @@ if (typeof window !== "undefined") {
 export function PHProvider({ children }: { children: React.ReactNode }) {
   // Disable analytics solution from here
   // https://posthog.com/questions/how-can-i-disable-post-hog-on-certain-environments
-  const analyticsEnabled =
-    JSON.parse(window.localStorage.getItem("analytics_allowed")!) === 1
-      ? true
-      : false;
+  const [analyticsEnabled, setAnalyticsEnabled] = useState<boolean | null>(
+    null,
+  );
 
-  if (!analyticsEnabled) return <>{children}</>;
+  useEffect(() => {
+    const enabled =
+      JSON.parse(localStorage.getItem("analytics_allowed") || "false") === 1;
+    setAnalyticsEnabled(enabled);
+  }, []);
+
+  if (analyticsEnabled === null) {
+    // Still determining if analytics are enabled
+    return <>{children}</>;
+  }
+
+  if (!analyticsEnabled) {
+    // Analytics are disabled
+    return <>{children}</>;
+  }
 
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
