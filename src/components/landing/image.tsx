141e82fffb46e381fe6536908cb1e99a013a6e45
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-10-19 12:02:12 +1100

    Images now centered properly.

--- src/components/landing/image.tsx
+++ src/components/landing/image.tsx
@@ -1,39 +1,43 @@
-"use client";
+'use client'
 
-import BoxReveal from "@/components/magicui/box-reveal";
-import DotPattern from "@/components/magicui/dot-pattern";
-import { TopNavbar } from "@/components/top-navbar";
-import { Button } from "@/components/ui/Button";
-import { cn } from "@/lib/utils";
-import { BorderBeam } from "../magicui/border-beam";
-import Image from "next/image";
-import { useTheme } from "next-themes";
+import BoxReveal from '@/components/magicui/box-reveal'
+import DotPattern from '@/components/magicui/dot-pattern'
+import { TopNavbar } from '@/components/top-navbar'
+import { Button } from '@/components/ui/Button'
+import { cn } from '@/lib/utils'
+import { BorderBeam } from '../magicui/border-beam'
+import Image from 'next/image'
+import { useTheme } from 'next-themes'
 import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-  CardFooter,
-} from "../ui/card";
-import { AspectRatio } from "../ui/aspect-ratio";
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardContent,
+    CardFooter,
+} from '../ui/card'
+import { AspectRatio } from '../ui/aspect-ratio'
 
 export default function HeroImage() {
-  const { theme, setTheme } = useTheme();
+    const { theme, setTheme } = useTheme()
 
-  return (
-    <div className="flex relative h-lvh w-[80rem] items-center justify-center mt-16">
-      <Image
-        src={theme === "light" ? "/hero/app-light.jpeg" : "/hero/app.jpeg"}
-        alt="image"
-        fill
-        className="object-contain object-left-top absolute rounded-lg inset-0 "
-        // width={0}
-        // height={0}
-        // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        // style={{ width: "80lvw", height: "auto" }} // optional
-      />
-      {/* <BorderBeam size={250} duration={12} delay={9} /> */}
-    </div>
-  );
+    return (
+        <div className="flex relative h-lvh w-[80rem] items-center justify-center mt-16">
+            <Image
+                src={
+                    theme === 'light'
+                        ? '/hero/app-light.jpeg'
+                        : '/hero/app.jpeg'
+                }
+                alt="image"
+                // fill
+                className="rounded-lg"
+                width={0}
+                height={0}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                style={{ width: '80lvw', height: 'auto' }} // optional
+            />
+            {/* <BorderBeam size={250} duration={12} delay={9} /> */}
+        </div>
+    )
 }
