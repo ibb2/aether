5026e40e95ee509dd534bf73491ef4980ab01c03
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-10-19 12:13:55 +1100

    Tweaked homepage again.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
-import { BentoFeatures } from "@/components/landing/FeatureOne";
-import { Action } from "@/components/landing/FeatureTwo";
-import Hero from "@/components/landing/hero";
-import HeroImage from "@/components/landing/image";
-import BoxReveal from "@/components/magicui/box-reveal";
-import DotPattern from "@/components/magicui/dot-pattern";
-import { TopNavbar } from "@/components/top-navbar";
-import { cn } from "@/lib/utils";
+import { BentoFeatures } from '@/components/landing/FeatureOne'
+import { Action } from '@/components/landing/FeatureTwo'
+import Hero from '@/components/landing/hero'
+import HeroImage from '@/components/landing/image'
+import BoxReveal from '@/components/magicui/box-reveal'
+import DotPattern from '@/components/magicui/dot-pattern'
+import { TopNavbar } from '@/components/top-navbar'
+import { cn } from '@/lib/utils'
 
 export default function Homepage() {
-  return (
-    <div className="flex flex-col items-center justify-center py-16">
-      <TopNavbar />
-      <DotPattern
-        className={cn(
-          "[mask-image:radial-gradient(700px_circle_at_center,white,transparent)]",
-        )}
-      />
-      <Hero />
-      <HeroImage />
-      <BentoFeatures />
-      <Action />
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center py-16 gap-y-32">
+            <TopNavbar />
+            <DotPattern
+                className={cn(
+                    '[mask-image:radial-gradient(700px_circle_at_center,white,transparent)]'
+                )}
+            />
+            <Hero />
+            <HeroImage />
+            <BentoFeatures />
+            <Action />
+        </div>
+    )
 }
--- src/components/landing/image.tsx
+++ src/components/landing/image.tsx
@@ -1,28 +1,13 @@
 'use client'
 
-import BoxReveal from '@/components/magicui/box-reveal'
-import DotPattern from '@/components/magicui/dot-pattern'
-import { TopNavbar } from '@/components/top-navbar'
-import { Button } from '@/components/ui/Button'
-import { cn } from '@/lib/utils'
-import { BorderBeam } from '../magicui/border-beam'
 import Image from 'next/image'
 import { useTheme } from 'next-themes'
-import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardDescription,
-    CardContent,
-    CardFooter,
-} from '../ui/card'
-import { AspectRatio } from '../ui/aspect-ratio'
 
 export default function HeroImage() {
     const { theme, setTheme } = useTheme()
 
     return (
-        <div className="flex relative h-lvh w-[80rem] items-center justify-center mt-16">
+        <div className="flex relative h-lvh w-[80rem] items-center justify-center">
             <Image
                 src={
                     theme === 'light'
@@ -30,14 +15,12 @@ export default function HeroImage() {
                         : '/hero/app.jpeg'
                 }
                 alt="image"
-                // fill
                 className="rounded-lg"
                 width={0}
                 height={0}
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 style={{ width: '80lvw', height: 'auto' }} // optional
             />
-            {/* <BorderBeam size={250} duration={12} delay={9} /> */}
         </div>
     )
 }
