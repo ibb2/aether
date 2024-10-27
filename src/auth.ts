455724d0b1f64e8409e132416e41ec9e7ef90a24
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-10-27 20:06:03 +1100

    Set up drizzle and turso again. Got Auth Js to work finally.

---
+++ src/auth.ts
@@ -0,0 +1,39 @@
+import NextAuth from 'next-auth'
+import GitHub from 'next-auth/providers/github'
+import Google from 'next-auth/providers/google'
+import Resend from 'next-auth/providers/resend'
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import { db } from './db/drizzle/index'
+import {
+    users,
+    accounts,
+    sessions,
+    verificationTokens,
+} from './db/drizzle/schema'
+import type { Provider } from 'next-auth/providers'
+
+const providers: Provider[] = [GitHub, Resend, Google]
+
+export const providerMap = providers
+    .map((provider) => {
+        if (typeof provider === 'function') {
+            const providerData = provider()
+            return { id: providerData.id, name: providerData.name }
+        } else {
+            return { id: provider.id, name: provider.name }
+        }
+    })
+    .filter((provider) => provider.id !== 'credentials')
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: DrizzleAdapter(db, {
+        usersTable: users,
+        accountsTable: accounts,
+        sessionsTable: sessions,
+        verificationTokensTable: verificationTokens,
+    }),
+    providers: [GitHub, Google, Resend],
+    pages: {
+        signIn: '/sign-in',
+    },
+})
