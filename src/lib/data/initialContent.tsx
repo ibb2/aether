b11b74e7e90bc3a21863cdf481629bb5bcc72396
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-01-27 00:24:49 +1100

    Fixed initial note error
    Caused double headers.

--- src/lib/data/initialContent.tsx
+++ src/lib/data/initialContent.tsx
@@ -2,14 +2,6 @@ export const initialContent = {
     type: 'doc',
     content: [
         {
-            type: 'heading',
-            attrs: {
-                class: null,
-                textAlign: 'left',
-                level: 1,
-            },
-        },
-        {
             type: 'paragraph',
             attrs: {
                 class: null,
