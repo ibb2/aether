9399948285e1f2299f2780977a9f6bdd864bae9b
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-04-28 21:22:37 +1000

    Installed better auth

--- src/components/top-navbar.tsx
+++ src/components/top-navbar.tsx
@@ -14,7 +14,10 @@ import { useSession } from 'next-auth/react'
 export function TopNavbar() {
     const { data: session } = useSession()
 
-    const callbackUrl = 'https://preview.aethernotes.ink'
+    const callbackUrl =
+        process.env.NODE_ENV === 'production'
+            ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
+            : 'http://localhost:3000'
 
     return (
         <nav className="fixed flex justify-center items-center w-lvw top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90">
