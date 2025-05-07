07a303c8e3fc8097ba1b11d8f62e86ac51125b62
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-05-07 16:26:05 +1000

    Tweaked image sizing.

--- src/components/landing/image.tsx
+++ src/components/landing/image.tsx
@@ -7,23 +7,53 @@ export default function HeroImage() {
     const { resolvedTheme } = useTheme()
 
     return (
-        <Image
-            src={
-                resolvedTheme === 'light'
-                    ? '/hero/app-light.png'
-                    : '/hero/app.png'
-            }
-            alt="image"
-            className="flex relative h-lvh max-w-5xl items-center justify-center rounded-lg"
-            width={0}
-            height={0}
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 60vw, 33vw"
-            // style={{ width: 'auto', height: 'auto' }} // optional
-            // sizes="100vw"
-            style={{
-                width: '100%',
-                height: 'auto',
-            }}
-        />
+        <div className="w-2/3 xl:w-5/6">
+            <Image
+                src={
+                    resolvedTheme === 'light'
+                        ? '/hero/app-light.png'
+                        : '/hero/app.png'
+                }
+                className="flex relative h-lvh items-center justify-center rounded-lg"
+                alt="Hero Image"
+                width={1920}
+                height={1080}
+                // sizes="100vh"
+                quality={100}
+                style={{
+                    width: '100%',
+                    height: 'auto',
+                }}
+                layout="responsive"
+            />
+        </div>
+        // <Image
+        //     src={
+        //         resolvedTheme === 'light'
+        //             ? '/hero/app-light.png'
+        //             : '/hero/app.png'
+        //     }
+        //     alt="image"
+        //     className=""
+        //     width={0}
+        //     height={0}
+        //     sizes="(max-width: 768px) 100vw, (max-width: 1024px)100vw, (max-width: 1200px) 100vw, 100vw"
+        //     // style={{ width: 'auto', height: 'auto' }} // optional
+        //     // sizes="100vw"
+        //     style={{
+        //         width: '100%',
+        //         height: 'auto',
+        //     }}
+        //     quality={100}
+        //     priority
+        // />
+        // <image
+        //    priority
+        //    src="{IMAGE_URL}"
+        //    layout="fill"
+        //    objectFit="cover"
+        //    objectPosition="center"
+        //    alt="hero image example"
+        //  />
     )
 }
