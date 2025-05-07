5ed36e6fd5a20c4fa9366d8fc8fbe6ceae88982f
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-05-07 15:50:56 +1000

    Changed Images and respsonsiveness

--- src/components/landing/image.tsx
+++ src/components/landing/image.tsx
@@ -7,20 +7,24 @@ export default function HeroImage() {
     const { resolvedTheme } = useTheme()
 
     return (
-        <div className="flex relative h-lvh w-[80rem] items-center justify-center">
-            <Image
-                src={
-                    resolvedTheme === 'light'
-                        ? '/hero/app-light.jpeg'
-                        : '/hero/app.jpeg'
-                }
-                alt="image"
-                className="rounded-lg"
-                width={0}
-                height={0}
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                style={{ width: '80lvw', height: 'auto' }} // optional
-            />
-        </div>
+        <Image
+            src={
+                resolvedTheme === 'light'
+                    ? '/hero/app-light.png'
+                    : '/hero/app.png'
+            }
+            alt="image"
+            className="flex relative h-lvh max-w-5xl items-center justify-center rounded-lg"
+            width={0}
+            height={0}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            // style={{ width: 'auto', height: 'auto' }} // optional
+            // sizes="100vw"
+            style={{
+                width: '100%',
+                height: 'auto',
+            }}
+            quality={100}
+        />
     )
 }
