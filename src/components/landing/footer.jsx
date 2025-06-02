b46ca5d5d8e05881fec18bf4b12bac0a0a651660
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-06-02 18:31:02 +1000

    moved svg jsx to new file.

--- src/components/landing/footer.jsx
+++ src/components/landing/footer.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import AetherIconSvg from '@/assets/aether-icon'
+import AetherIconSvg from '@/assets/aicon'
 
 export default function FooterSection() {
     return (
